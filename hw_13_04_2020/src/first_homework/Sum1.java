6baa839c60f9c07f46c7e0ced2fb99d553beef66
artemMovsesyantss <63604276+artemMovsesyantss@users.noreply.github.com>
2020-04-13 20:28:25 +0300

    Update Sum1.java

--- hw_13_04_2020/src/first_homework/Sum1.java
+++ hw_13_04_2020/src/first_homework/Sum1.java
@@ -2,14 +2,14 @@ package first_homework;
 
 public class Sum1 {
     public static int Metod(int a, int b) {//Calculating the sum of long integers
-        int sum01 = a + b;
+        int sum01 = a + b; // лишняя переменная sum01, можно сразу return a + b;
         return sum01;
     }
 
-    public static void Test(int a) {
+    public static void Test(int a) { // добавь фигурные скобки для if и else
         if (a == 4) System.out.println("test completed successfully");//testing sum4
         else System.out.println("test failed");
-    }
+    } 
 
     public static void main(String[] args) {
         System.out.println("sum2=" + Metod(2, 2));
