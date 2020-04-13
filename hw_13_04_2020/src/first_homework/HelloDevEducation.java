bcaadbdd151e6d5627eef9411e46f6f31130a67f
Tarasov Ivan <mr.wilfor@gmail.com>
2020-04-13 17:20:56 +0300

    correction

---
+++ hw_13_04_2020/src/first_homework/HelloDevEducation.java
@@ -0,0 +1,7 @@
+package first_homework;
+
+public class HelloDevEducation {
+    public static void main(String[] args) {
+        System.out.println("Hello, DevEducation!");
+    }
+}
--- hw_13_04_2020/src/first_homework/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package first_homework;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
-
